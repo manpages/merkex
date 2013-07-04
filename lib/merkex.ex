d1242d57b831182930e115a0cfd8a6708960ca1c
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-07-04 18:21:22 +0300

    Use defrecord instead of defrecordp (we want to implement access protocol!

--- lib/merkex.ex
+++ lib/merkex.ex
@@ -1,15 +1,13 @@
 defmodule Merkex do
-  defrecordp :tree, gb: :gb_trees.empty(), width: 0
+  defrecord Tree, gb: :gb_trees.empty(), width: 0
   defrecordp :data, id: 0, binary: ""
 
-  defrecordp :state, [ tree: {Merkex, :gb_trees.empty(), 0},
+  defrecordp :state, [ tree: Tree.new(),
                        data: :ordsets.new() ]
 
   @spec new(list(term())) :: tuple( atom(), #state
-                                   tuple( atom(), #tree
-                                          tuple(non_neg_integer(), tuple()), #gb_tree
-                                          non_neg_integer() ), #width
-                                   tuple( atom(), #data
+                                    Tree.t(),
+                                    tuple( atom(), #data
                                           list(tuple(atom(), non_neg_integer(), binary())) ) )
   def new([]),     do: state()
   def new([x|xs]), do: insert(x, new(xs))
