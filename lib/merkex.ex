49c7c0dbe8fb2d4260dfe662318e4203d49f2fcb
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-07-05 17:34:49 +0300

    Use ordinary list to keep data

--- lib/merkex.ex
+++ lib/merkex.ex
@@ -3,18 +3,19 @@ defmodule Merkex do
   defrecordp :data, id: 0, binary: ""
 
   defrecordp :state, [ tree: Tree.new(),
-                       data: :ordsets.new() ]
+                       data: [] ]
 
   @spec new(list(term())) :: tuple( atom(), #state
                                     Tree.t(),
                                     tuple( atom(), #data
-                                           list(tuple(atom(), non_neg_integer(), binary())) ) )
-  def new([]),     do: state()
-  def new([x|xs]), do: insert(x, new(xs))
+                                           list(binary()) ) )
+  def new(x) when is_list(x), do: new1(:lists.reverse x)
+  def new1([]),     do: state()
+  def new1([x|xs]), do: insert(x, new1(xs))
 
-  def insert(_, tree) do 
-    data = state(tree, :data)
-    size = :ordsets.size(data)
-    state(tree, data: :ordsets.add_element({size, nil}, data))
+  def insert(x, s) do
+    Tree[gb: gb, width: w] = state(s, :tree)
+    gb = :gb_trees.insert({0, w}, term_to_binary(x), gb)
+    state(tree: Tree[gb: gb, width: w+1], data: [x|state(s, :data)])
   end
 end
