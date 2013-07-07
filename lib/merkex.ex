dc6980f978a12084fcd28c3daab023b3a76d87d0
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-07-07 16:07:34 +0300

    Implement Merkle tree.

--- lib/merkex.ex
+++ lib/merkex.ex
@@ -13,17 +13,34 @@ defmodule Merkex do
     Tree[gb: gb, width: w] = state(s, :tree)
     data = term_to_binary(x)
     gb = :gb_trees.insert({0, w}, :crypto.hash(:sha, data), gb)
-    enter(state(tree: Tree[gb: gb, width: w+1], data: [data|state(s, :data)]))
+    state(tree: update(0, w+1, Tree[gb: gb, width: w+1]), data: [data|state(s, :data)])
   end
 
   defp new1([]),     do: state()
   defp new1([x|xs]), do: insert(x, new1(xs))
 
-  defp enter(x), do: x
+  defp update(_, 1, t), do: t
+  defp update(h, l, Tree[gb: gb, width: w]) do
+    l1 = trunc(l/2) + rem(l,2) # amount of elements at height = h+1
+
+    if rem(l,2) == 1 do # if current amount of elements is odd
+      update(h+1, l1, Tree[ gb: :gb_trees.enter( {h+1, trunc(l/2)},
+                                                 :gb_trees.get({h,l-1}, gb),
+                                                  gb ),
+                            width: w ])
+    else # if current amount of elements is even
+      left  = :gb_trees.get({h,l-1}, gb) # l-1 = id of the last element of height = h
+      right = :gb_trees.get({h,l-2}, gb)
+      update(h+1, l1, Tree[ gb: :gb_trees.enter( {h+1, trunc(l/2)-1},
+                                                 :crypto.hash(:sha, [left, right]),
+                                                 gb ),
+                            width: w ])
+    end
+  end
 
   defimpl Access, for: Tree do
     def access(Tree[gb: gb, width: w], {x,y}) do
-      if y > trunc(w/(x+1)) or x > trunc(w/2)+1, do: nil, else: :gb_trees.get {x, y}, gb
+      if y > trunc(w/(x+1)) or x > trunc(w/2)+1, do: nil, else: :gb_trees.get({x, y}, gb)
     end
   end
 end
