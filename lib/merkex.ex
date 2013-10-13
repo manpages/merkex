b2c58dfd25982047821ca7e7251d941539865583
Jonn Mostovoy <amarr.industrial@gmail.com>
2013-10-14 01:51:11 +0300

    Make hash function configurable.

--- lib/merkex.ex
+++ lib/merkex.ex
@@ -1,23 +1,23 @@
 defmodule Merkex do
-  defrecord Tree, gb: :gb_trees.empty(), width: 0
+  defrecord Tree, gb: :gb_trees.empty(), width: 0, hash_function: nil
   defrecordp :data, id: 0, binary: ""
 
   defrecordp :state, [ tree: Tree.new(),
                        data: [] ]
 
-  @spec new(list(term())) :: {atom, Tree.t, {atom, [binary()]}}
-  def new(x) when is_list(x), do: new1(:lists.reverse x)
+  @spec new(list(term()), fun()) :: {atom, Tree.t, {atom, [binary()]}}
+  def new(x, f // &(:crypto.hash(:sha, &1))) when is_list(x), do: new1(:lists.reverse(x), f)
 
-  @spec insert(term, {atom, Tree.t, {atom, [binary()]}}) :: {atom, Tree.t, {atom, [binary()]}}
-  def insert(x, s) do
+  @spec insert(term, {atom, Tree.t, {atom, [binary()]}}, fun()) :: {atom, Tree.t, {atom, [binary()]}}
+  def insert(x, s, f) do
     Tree[gb: gb, width: w] = state(s, :tree)
     data = term_to_binary(x)
-    gb = :gb_trees.insert({0, w}, :crypto.hash(:sha, data), gb)
+    gb = :gb_trees.insert({0, w}, f.(data), gb)
     state(tree: update(0, w+1, Tree[gb: gb, width: w+1]), data: [data|state(s, :data)])
   end
 
-  defp new1([]),     do: state()
-  defp new1([x|xs]), do: insert(x, new1(xs))
+  defp new1([],     f), do: new_state(f)
+  defp new1([x|xs], f), do: insert(x, new1(xs, f), f)
 
   defp update(_, 1, t), do: t
   defp update(h, l, Tree[gb: gb, width: w]) do
@@ -36,6 +36,12 @@ defmodule Merkex do
                                                  gb ),
                             width: w ])
     end
+  end
+
+  defp new_state(f) do 
+    state(tree: tree) = state
+    tree = tree.hash_function(f)
+    state(state, tree: tree)
   end
 
   defimpl Access, for: Tree do
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Merkex.Mixfile do
   def project do
     [ app: :merkex,
       version: "0.0.1",
-      elixir: "~> 0.9.4-dev",
+      elixir: "~> 0.10.4-dev",
       deps: deps ]
   end
 
